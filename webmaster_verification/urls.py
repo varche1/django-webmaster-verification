e43378a9556da42c1d798d3154a0036621e9f359
Cihan Okyay <cihan.okyay@metglobal.com>
2015-02-04 11:28:28 +0200

    changed yandex verification file tyoe

--- webmaster_verification/urls.py
+++ webmaster_verification/urls.py
@@ -6,32 +6,33 @@ from .views import BingVerificationView
 from .views import MajesticVerificationView
 from .views import YandexVerificationView
 from .views import AlexaVerificationView
+from .views import RobotsView
 
 
 urlpatterns = patterns('',
     url(
         r'^google(?P<code>[0-9a-f]{16})\.html$',
         GoogleVerificationView.as_view(),
-        name = 'google_verify'
+        name='google_verify'
     ),
     url(
         r'^BingSiteAuth\.xml$',
         BingVerificationView.as_view(),
-        name = 'bing_verify'
+        name='bing_verify'
     ),
     url(
         r'^MJ12_(?P<code>[0-9A-F]{32})\.txt$',
         MajesticVerificationView.as_view(),
-        name = 'majestic_verify'
+        name='majestic_verify'
     ),
     url(
-        r'^yandex_(?P<code>[0-9a-f]{16})\.txt$',
+        r'^yandex_(?P<code>[0-9a-f]{16})\.html$',
         YandexVerificationView.as_view(),
-        name = 'majestic_verify'
+        name='yandex_verify'
     ),
     url(
         r'^(?P<code>[0-9a-zA-Z]{27})\.html$',
         AlexaVerificationView.as_view(),
-        name = 'alexa_verify'
+        name='alexa_verify'
     ),
 )
